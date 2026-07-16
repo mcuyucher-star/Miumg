fd18d22a46f8c2db47c8579c2a1cc5b04d23629d
Maynor Cuyuche <mcuyucher@miumg.edu.com>
2026-07-16 15:19:27 -0600

    Mostrra suma de numeros 1-100

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,12 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola, mundo!");
+
+        int suma = 0;
+        for (int i = 1; i <= 100; i++) {
+            suma += i;
+        }
+
+        System.out.println("La suma de 1 a 100 es: " + suma);
     }
 }
